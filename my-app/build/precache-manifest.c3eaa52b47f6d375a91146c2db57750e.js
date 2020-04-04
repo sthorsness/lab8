02d6f592a9a07f1adeea3e314ca94c7488cf95e4
sthorsness <sgthorsness@gmail.com>
2020-04-04 20:09:19 +0000

    final

---
+++ my-app/build/precache-manifest.c3eaa52b47f6d375a91146c2db57750e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5083a5154c079610a7f44fa670828c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e298559abf54fe0ee5e",
+    "url": "/static/css/main.f1da3ae8.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e298559abf54fe0ee5e",
+    "url": "/static/js/main.e79856a3.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "b125cd193cfcd8550a8adb935b7cca84",
+    "url": "/static/media/cat.b125cd19.jpg"
+  }
+]);
